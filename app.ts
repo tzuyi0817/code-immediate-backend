f0f28b3dcc5aa9a33e1d10f74574bc81423a0275
tzuyi <a0988426059@gmail.com>
2022-10-27 16:53:21 +0800

    add express-session

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import session from 'express-session';
 import { urlencoded, json } from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -14,6 +15,11 @@ const app = express();
 app.use(urlencoded({ extended: false }));
 app.use(json());
 app.use(cors());
+app.use(session({
+  secret: 'code-immediate',
+  resave: false,
+  saveUninitialized: true,
+}));
 app.use(userRouter);
 app.use(codeRouter);
 app.use(passport.initialize());
